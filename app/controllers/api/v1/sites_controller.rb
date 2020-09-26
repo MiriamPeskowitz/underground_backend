594a472714d47533242a2d3d69ef0d983707d91d
MiriamPeskowitz <miriam5000@gmail.com>
2020-09-25 23:25:48 -0400

    Add routes and methods for create and update to SitesController

--- app/controllers/api/v1/sites_controller.rb
+++ app/controllers/api/v1/sites_controller.rb
@@ -3,4 +3,29 @@ class Api::V1::SitesController < ApplicationController
 		sites = Site.all
 		render json: SiteSerializer.new(sites)
 	end
+
+	def show 
+		site = Site.find(params[:id])
+		render json: SiteSerializer.new(site)
+	end
+
+
+	def update
+    @site.update(site_params)
+    if @site.save
+      render json: @site, status: :accepted
+    else
+      render json: { errors: @site.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+
+	def create
+		site = Site.new(site_params)
+		if site.save 
+			render json: SiteSerializer.new(site), status: :accepted 
+		else 
+			render json: {errors: site.errors.full_messages}, status: :unprocessible_entity
+		end
+	end
 end
